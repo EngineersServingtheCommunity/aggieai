618f2267c9ee9c3b3cc7e59e92adbb5c79b38155
Jaxson Mercer <jaxmerc03@gmail.com>
2023-02-17 20:19:35 -0600

    Updated Routes

--- aggieai-client/src/index.js
+++ aggieai-client/src/index.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
-import Classes from "./pages/Classes"
+import Classes from "./pages/Classes.js"
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -12,7 +13,8 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/classes" element={<Classes/>} />
+          <Route path="/classes" element={<Classes />} />
+          <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );

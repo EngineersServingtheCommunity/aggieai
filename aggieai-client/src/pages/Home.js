576686536da2befb9d8fdb9490783732592197a1
Jaxson Mercer <jaxmerc03@gmail.com>
2023-02-17 20:20:27 -0600

    updated page structure

--- aggieai-client/src/pages/Home.js
+++ aggieai-client/src/pages/Home.js
@@ -10,6 +10,7 @@ function Home() {
         <div>
             <p>This will be our home page</p>
             <button onClick={()=>navigate('about')}>Go to about page</button>
+            <button onClick={()=>navigate('classes')}>Go to classes page</button>
         </div>
     );
 }

bcae492121f9b109b1d7522e4e853a1cc9f5f82b
Robbie <rdcl@tamu.edu>
2023-04-13 19:48:24 -0500

    added jsx to home.js

--- aggieai-client/src/pages/Home.js
+++ aggieai-client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import './Home.css';
 
 const classURL  ='http://localhost:1337/api/classes'
 
@@ -19,15 +20,30 @@ function Home() {
       }, []);
 
     
-    if (!classList) return <div><p>nothing found</p></div>;
+    //if (!classList) return <div><p>nothing found</p></div>;
+    //let first_class = classList[0].attributes.name;
 
     return (
         <div>
-            <h1>Class page, classes available</h1>
-            <p>{classList[0].attributes.name}</p>
-
-            <button onClick={()=>navigate('about')}>Go to about page</button>
-            <button onClick={()=>navigate('classes')}>Go to classes page</button>
+            <title>Aggie AI</title>
+            <link rel="stylesheet" href="https://use.typekit.net/nbf0pnf.css" />
+            <link rel="stylesheet" href="Home.css" />
+            <nav>
+                <ul>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+                <li>
+                    <a href="#">ESC</a>
+                </li>
+                </ul>
+            </nav>
+            <h1>Search. Rate. Gig'em.</h1>
+            <p>
+                Find your courses within the Aggie community <br />
+                system that has been through it all.
+            </p>
+            <button className="button">SEARCH FOR A CLASS</button>
         </div>
     );
 }

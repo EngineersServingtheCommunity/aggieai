150940d4b440dab3d47fb898a291aec5bdbe00a4
Jaxson Mercer <jaxmerc03@gmail.com>
2023-04-13 19:39:39 -0500

    all ratings for a class

--- aggieai-client/src/pages/Classes.js
+++ aggieai-client/src/pages/Classes.js
@@ -1,43 +1,59 @@
 import React from "react";
 import {useState, useEffect} from "react";
-import {useNavigate} from "react-router-dom";
 import './Classes.css';
 import Navbar from '../components/Navbar.js'
 import WriteReview from "../components/writeReview.js";
+import Comment from '../components/comment.js'
 import axios from 'axios';
 
-const baseURL  ='http://localhost:1337/api/classes?populate[ratings][populate][0]=comments'
+const classBaseURL  ='http://localhost:1337/api/classes'
+const ratingBaseURL = 'http://localhost:1337/api/ratings'
 
 function Classes() {
-    const navigate = useNavigate();
 
-    const [content, setContent] = useState(null);
+    const [classContent, setClassContent] = useState(null);
+    const [ratingContent, setRatingContent] = useState(null);
+    const [totRating, setTotRating] = useState(null);
 
     useEffect(() => {
-        axios.get(baseURL)
+        axios.get(classBaseURL)
         .then((response) => {
-            console.log(response)
-            setContent(response.data);
+            console.log(response);
+            setClassContent(response.data);
         })
         .catch(error => console.log(error));
+
+        axios.get(ratingBaseURL)
+        .then((response) => {
+            setRatingContent(response.data);
+            setTotRating(response.data.data.length);
+        })
+        .catch(error => console.log(error))
       }, []);
 
+        let itemList=[];
+        for(let i=0; i<totRating; i++){
+            console.log(ratingContent.data[i].attributes.grade)
+            itemList.push( <Comment review={ratingContent.data[i].attributes}/>)
+        }
 
-    if (!content) return <div><p>nothing found</p></div>;
+    if (!classContent) return <div><p>nothing found</p></div>;
 
     return (
         <div>
             <div>
                 <Navbar/>
             </div>
-            <h1>{content.data[0].attributes.name}</h1>
-            <p>{content.data[0].attributes.description}</p>
+            <h1>{classContent.data[0].attributes.name}</h1>
+            <p>{classContent.data[0].attributes.description}</p>
+            
+            <div>
+                {itemList}
+            </div>
 
             <div id='thing'>
                 <WriteReview/>
             </div>
-
-            
         </div>
     );
 }
